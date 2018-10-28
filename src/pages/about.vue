76c2c04fbb8624fe6ba7a0c50526006ec26b2ed0
Andrew K <ka-spb@yandex.ru>
2018-10-28 17:54:50 +0300

    Deploy in Heroku

---
+++ src/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>О нас</p>
+    <router-link tag="a" to="/">Главная</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "about"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+    <p>Главная</p>
+    <router-link tag="a" to="/about" class="test">О нас</router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
